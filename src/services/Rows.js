5832f44e4fa76639b7d54b496a06479be342562d
victips <torvicarias@gmail.com>
2022-01-05 08:18:27 -0600

    Se crea función Rows para generar los datos que alimentarán la tabla de amortización

---
+++ src/services/Rows.js
@@ -0,0 +1,24 @@
+function Rows(loan, nper, rate) {
+  let principal = "";
+  if (loan && nper && rate !== "") {
+    principal = loan / nper;
+  }
+
+  let rows = [
+    { period: 0, payment: 0, interest: 0, principal: 0, balance: loan },
+  ];
+  if (loan && nper && rate !== "") {
+    for (let period = 1; period <= nper; period++) {
+      rows.push({
+        period: period,
+        payment: principal + (loan - (loan / nper) * (period - 1)) * rate / 100,
+        interest: (loan - (loan / nper) * (period - 1)) * rate / 100,
+        principal: principal,
+        balance: loan - (loan / nper) * period,
+      });
+    }
+  }
+  return rows;
+}
+
+export default Rows;
